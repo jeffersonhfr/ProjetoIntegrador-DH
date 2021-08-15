823c2c4e0c03db35552e94f6a8186b71975d29da
Alysson de Jesus Alves <alyssonja23@gmail.com>
2021-08-15 14:50:16 -0300

    feature: incrementado estilo do bloco main da página

--- index/src/index.js
+++ index/src/index.js
@@ -4,7 +4,7 @@ let viagens = []
 
 
 
-function adicionarTema(Tema) {
+function adicionarTemaI(Tema) {
     for (let i = 0; i<ViagensTematicas.length; i++){
         ViagensTematicas.item(i).innerHTML += `<li><img src= "${Tema.src}"/><li>`
     }
