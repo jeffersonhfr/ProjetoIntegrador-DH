7bb2858aed4fadf0a735471ac6446432581f5d09
Alysson de Jesus Alves <alyssonja23@gmail.com>
2021-08-16 03:29:04 -0300

    feature: Objeto para representar a lista de temas criado

--- index/src/index.js
+++ index/src/index.js
@@ -1,4 +1,30 @@
 ///let ViagensTematicas = document.getElementsByClassName("ViagensTematicas");
 
-
+    
+let ListaDeTemas = {
+    temas: [],
+    setTemas: function (temas) {
+        this.temas = temas
+    },
+    getTemas: function () {
+        return this.temas
+    },
+    postTemas: function (tema) {
+        this.getTemas().push(tema)
+    },
+    putTemas: function (tema, index) {
+        this.getTemas()[index] = tema
+    },
+    deleteTemas: function (index) {
+        let response = [];
+        this.getTemas()[index] = null;
+        this.getTemas().map(tema => {
+            if (tema != null) {
+                response.push(tema)
+            }  
+        });
+        this.setTemas(response);
+    }
+    
+}
 
