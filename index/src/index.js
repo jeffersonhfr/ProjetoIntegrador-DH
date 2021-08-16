1f9d66cd1f0bd003975e8b4d52aed4f8fc1b90f3
Alysson de Jesus Alves <alyssonja23@gmail.com>
2021-08-16 07:22:12 -0300

    bugfix: corrigido problema ao exibir lista de temas com icones e sem icones

--- index/src/index.js
+++ index/src/index.js
@@ -1,4 +1,3 @@
-///let ViagensTematicas = document.getElementsByClassName("ViagensTematicas");
 
     
 let ListaDeTemas = {
@@ -27,4 +26,50 @@ let ListaDeTemas = {
     }
     
 }
+
+ListaDeTemas.setTemas([
+    {
+        nome: "Aventura",
+        class: "Aventura",
+        src: "./index/assets/img/tema-aventura-icon.jpg"
+    },
+    
+    {
+        nome: "Romântico",
+        class: "Romantico",
+        src:"./index/assets/img/tema-romantico-icon.jpg"
+    },
+    {
+        nome: "Balada",
+        class: "Balada",
+        src:"./index/assets/img/tema-balada-icon.jpg"
+    },
+    {
+        nome: "Praia",
+        class: "Praia",
+        src:"./index/assets/img/tema-praia-icon.jpg"
+    }, 
+    {
+        nome: "Neve",
+        class: "Neve",
+        src:"./index/assets/img/tema-neve-icon.jpg"
+    }
+
+]);
+
+function exibirIconesDeTemas() {
+    let temas = ListaDeTemas.getTemas()
+    let viagensTematicas = document.getElementsByClassName("ViagensTematicas ExibeIcone");
+    for (let i = 0; i < viagensTematicas.length; i++) {
+        for (tema of temas) {
+            viagensTematicas[i].innerHTML +=
+                `<li class = "Tema ${tema.class}">
+                     <a href = "/"><img class= "${tema.class} icone"src="${tema.src}"/></a>
+                <li>`
+        }
+    }
+}
+
+exibirIconesDeTemas();
+
 
