8a845be4f1e1fbb9fdbda3b524deaafb54f196c5
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2021-10-11 22:57:37 -0300

    feature: Criando e atualizando rotas de Login e Logout

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var loginContoller = require('../controllers/login')
+const authMiddleware = require('../middlewares/auth')
 
-/* GET home page. */
+
+// ROTAS DE LOGIN
 router.get('/', loginContoller.index);
+router.post('/', authMiddleware, loginContoller.auth) // Execução de login
+
+
+// // ROTAS DE CADATRO
+// router.get('/cadastro', loginContoller.cadastro); // Exibe rota cadastro
+// router.post('/cadastro', loginContoller.add) // Execução de cadastro
 
 module.exports = router;
