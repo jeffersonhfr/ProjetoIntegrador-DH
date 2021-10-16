c7d239225d94d4fbd8222f1a6fa65aef915cfaa1
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2021-10-16 17:54:55 -0300

    feature: Add rota e view Edit Sobre Nós

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,11 +2,14 @@ const express = require('express');
 const  router = express.Router()
 const  adminMiddleware = require('../middlewares/admin');
 const  ajudaController = require('../controllers/ajuda');
-// const  sobreController = require('../controllers/sobre');
+const  sobreController = require('../controllers/sobre');
 // const  pacotesController = require('../controllers/pacotes');
 
 //ROTAS ADMINISTRATIVAS (REQUEREM QUE O USUÁRIO AUTENTICADO SEJA ADMIN)
 router.get('/ajuda', adminMiddleware, ajudaController.list);
 router.post('/ajuda', adminMiddleware, ajudaController.update);
+
+router.get('/sobre', adminMiddleware, sobreController.list);
+router.post('/sobre', adminMiddleware, sobreController.update);
 
 module.exports = router
