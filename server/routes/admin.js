93dd72f98b6f85bc03e3af453556dbb732d256ea
Alysson de Jesus Alves <alyssonja23@gmail.com>
2021-10-16 16:43:57 -0300

    bugfix: corrigindo configuração da rota admin

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,7 +1,7 @@
 const express = require('express'),
   router = express.Router()
-   usersController = require('../controllers/users'),
-   acessoController = require('../controllers/acesso'),
+   //usersController = require('../controllers/users'),
+   admin = require('../controllers/admin'),
    adminMiddleware = require('../middlewares/admin')
 
 // ROTAS ADMINISTRATIVAS (REQUEREM QUE O USUÁRIO AUTENTICADO SEJA ADMIN)
