6fca08c88f7c8f6baab9b7b0c99b27e34f4fd0ae
Alysson de Jesus Alves <alyssonja23@gmail.com>
2021-10-17 12:05:42 -0300

    feature: configurando rota admin produto

--- server/controllers/pacotes.js
+++ server/controllers/pacotes.js
@@ -1,3 +1,4 @@
+const pacotes = require('../data/pacotes.json')
 const controller = {
   index: (req, res, next) => {
     res.render('pacotes', {
@@ -9,10 +10,19 @@ const controller = {
   show:(req,res,next)=>{
     res.render('pacote',{
       title: '| Pacote',
+      pacotes: pacotes,
       usuarioLogado: req.cookies.usuario,
       usuarioAdmin: req.cookies.admin
     })
   },
+  admin:(req,res,next)=>{
+      res.render('admin',{
+        title: '| admin',
+        pacotes: pacotes,
+        usuarioLogado: req.cookies.usuario,
+        usuarioAdmin: req.cookies.admin
+      })
+    }
 };
 
 module.exports = controller;
