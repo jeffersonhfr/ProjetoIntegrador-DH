cb1cf5a12b17a73c9448376e8a43e4b097901d18
Alysson de Jesus Alves <alyssonja23@gmail.com>
2021-10-18 14:09:32 -0300

    feature: criando interação da interface produtos com js

--- server/controllers/pacotes.js
+++ server/controllers/pacotes.js
@@ -1,16 +1,22 @@
 const pacotes = require('../data/pacotes.json')
 const controller = {
-  index: (req, res, next) => {
-    res.render('pacotes', {
-      title: '| Pacotes',
+  index:(req,res,next)=>{
+    res.render('pacotes',{
+      title: '| Pacote',
+      pacotes: pacotes,
+      valor: (valor) => {
+          return valor.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          })
+        },
       usuarioLogado: req.cookies.usuario,
       usuarioAdmin: req.cookies.admin
-    });
+    })
   },
   show:(req,res,next)=>{
     res.render('pacote',{
       title: '| Pacote',
-      pacotes: pacotes,
       usuarioLogado: req.cookies.usuario,
       usuarioAdmin: req.cookies.admin
     })
@@ -19,6 +25,12 @@ const controller = {
       res.render('admin',{
         title: '| admin',
         pacotes: pacotes,
+        valor: (valor) => {
+          return valor.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          })
+        },
         usuarioLogado: req.cookies.usuario,
         usuarioAdmin: req.cookies.admin
       })
