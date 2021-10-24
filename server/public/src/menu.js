4985165cfd36169e058c4a4bfb97de61be548fed
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2021-10-24 10:45:01 -0300

    bugfix: ajustando menu do usuario logado

--- server/public/src/menu.js
+++ server/public/src/menu.js
@@ -1,11 +1,15 @@
 var iconUser = document.querySelector('.menu__desk');
 var menuUser = document.querySelector('.menu__desk-conteudo');
+var menuHide = document.querySelector('.header_container');
 
-iconUser.addEventListener('mouseover',function() {
-  menuUser.classList.toggle('mostrar-menu');
-})
+// iconUser.addEventListener('click',function() {
+//   menuUser.classList.toggle('mostrar-menu');
+// })
 
-
-menuUser.addEventListener('mouseout', function() {
-  menuUser.classList.toggle('mostrar-menu');
-})
+window.addEventListener('click', function(e){   
+  if (document.querySelector('.menu__desk').contains(e.target)){
+    menuUser.classList.toggle('mostrar-menu');
+  } else {
+    menuUser.classList.remove('mostrar-menu');
+  }
+});
