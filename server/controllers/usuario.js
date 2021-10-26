9d9337c69b4f3f3192440a635349c36ed6a122e0
Alysson de Jesus Alves <alyssonja23@gmail.com>
2021-10-26 18:07:46 -0300

    feature: controller para atualizar os dados do usuário

--- server/controllers/usuario.js
+++ server/controllers/usuario.js
@@ -1,5 +1,6 @@
 const usuarios = require('../data/usuariosPlaceholder.json')
-
+const fs = require('fs'),
+path = require('path')
 const controller = {
     index: (req, res, next) => {
         let usuarioLogado = req.cookies.usuario;
@@ -19,8 +20,32 @@ const controller = {
         } else {
             res.redirect('../login')
         }   
+    }, edit: (req, res, next) => {
+        let usuarioLogado = req.cookies.usuario;
+        let usuario = usuarios.find(u => {
+            if (u.id === usuarioLogado.id) {
+                return u
+            }
+        });
+
+        usuario.nome = req.body.nome;
+        usuario.email = req.body.email;
+        usuario.nascimento = req.body.nascimento;
+        usuario.telefone = req.body.telefone;
+        usuario.cpf = req.body.cpf;
+        usuario.cep = req.body.cep;
+        usuario.endereco = req.body.endereco;
+        usuario.complemento = req.body.complemento;
+        usuario.cidade = req.body.cidade;
+        usuario.estado = req.body.uf;
+        usuario.senha = req.body.senha;
+        usuario.modificadoEm = new Date();
+        usuarios.push(usuario)
+        
+        res.redirect('../perfil');
+      
     },
-    edit: (req, res, next) => {
+    form_edit: (req, res, next) => {
         let usuarioLogado = req.cookies.usuario;
         let usuarioAdmin = req.cookies.admin;
         if (usuarioAdmin || usuarioLogado) {
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const usuarioController = require('../controllers/usuario');
 
 router.get('/', usuarioController.index);
-router.get('/edit', usuarioController.edit);
+
+router.get('/edit', usuarioController.form_edit);
+router.put('/edit', usuarioController.edit);
 
 module.exports = router;
