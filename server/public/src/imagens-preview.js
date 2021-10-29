136c84ae7a4d8c888c4eca28157da7263e510226
Alysson de Jesus Alves <alyssonja23@gmail.com>
2021-10-29 20:38:39 -0300

    feature: script para selecionar a imagem exibida

--- server/public/src/imagens-preview.js
+++ server/public/src/imagens-preview.js
@@ -1,2 +1,11 @@
 let imgContainer = document.querySelector('.Pacote-ImgContainer');
 let imgFirst = document.querySelector('.Pacote-ImgContainer__Img-first');
+let imgItem = document.querySelectorAll('.Pacote-ImgContainer__Img');
+let PrevSelected = imgItem[0];
+imgItem.forEach((item) => item.addEventListener('click', imgSelect));
+function imgSelect(item) {
+    selectedImg = item.target;
+    imgFirst.src = selectedImg.getAttribute('src');
+
+    
+}
