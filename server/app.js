7c217e96e1fbcef93dea78483eefc94dc270f716
Anna Raphaela Berto <raphaelaberto@gmail.com>
2021-10-29 13:43:28 -0300

    feature: adicionando rota de recuperar senha ao arquivo app

--- server/app.js
+++ server/app.js
@@ -1,53 +1,55 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const methodOverride = require('method-override');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const methodOverride = require("method-override");
 
-const adminMiddleware = require('./middlewares/admin');
-const logoutRouter = require('./routes/logout');
-const indexRouter = require('./routes/index');
-const pesquisaRouter = require('./routes/pesquisa');
-const pacotesRouter = require('./routes/pacotes');
-const sobreRouter = require('./routes/sobre');
-const ajudaRouter = require('./routes/ajuda');
-const loginRouter = require('./routes/login');
-const cadastroRouter = require('./routes/cadastro');
-const historicoRouter = require('./routes/historico');
-const adminRouter = require('./routes/admin');
-const perfilRouter = require('./routes/usuario');
+const adminMiddleware = require("./middlewares/admin");
+const logoutRouter = require("./routes/logout");
+const indexRouter = require("./routes/index");
+const pesquisaRouter = require("./routes/pesquisa");
+const pacotesRouter = require("./routes/pacotes");
+const sobreRouter = require("./routes/sobre");
+const ajudaRouter = require("./routes/ajuda");
+const recuperarSenhaRouter = require("./routes/recuperarSenha");
+const loginRouter = require("./routes/login");
+const cadastroRouter = require("./routes/cadastro");
+const historicoRouter = require("./routes/historico");
+const adminRouter = require("./routes/admin");
+const perfilRouter = require("./routes/usuario");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/style', express.static(__dirname + '/public/assets/style'));
-app.use('/fonts', express.static(__dirname + '/public/assets/fonts'));
-app.use('/img', express.static(__dirname + '/public/assets/img'));
-app.use('/src', express.static(__dirname + '/public/src'));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/style", express.static(__dirname + "/public/assets/style"));
+app.use("/fonts", express.static(__dirname + "/public/assets/fonts"));
+app.use("/img", express.static(__dirname + "/public/assets/img"));
+app.use("/src", express.static(__dirname + "/public/src"));
 
-app.use('/', indexRouter);
-app.use('/pesquisa', pesquisaRouter);
-app.use('/logout', logoutRouter);
-app.use('/pacotes', pacotesRouter);
-app.use('/sobre', sobreRouter);
-app.use('/ajuda', ajudaRouter);
-app.use('/login', loginRouter);
-app.use('/cadastro', cadastroRouter);
-app.use('/historico', historicoRouter);
-app.use('/perfil', perfilRouter);
+app.use("/", indexRouter);
+app.use("/pesquisa", pesquisaRouter);
+app.use("/logout", logoutRouter);
+app.use("/pacotes", pacotesRouter);
+app.use("/sobre", sobreRouter);
+app.use("/ajuda", ajudaRouter);
+app.use("/recuperarSenha", recuperarSenhaRouter);
+app.use("/login", loginRouter);
+app.use("/cadastro", cadastroRouter);
+app.use("/historico", historicoRouter);
+app.use("/perfil", perfilRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  res.status(404).render('404', {
-    title: '| Página não encontrada',
+  res.status(404).render("404", {
+    title: "| Página não encontrada",
     usuarioLogado: req.cookies.usuario,
     usuarioAdmin: req.cookies.admin,
   });
@@ -57,17 +59,17 @@ app.use(function (req, res, next) {
 app.use(adminMiddleware);
 
 // ROTAS ADMINISTRATIVAS
-app.use('/admin', adminRouter);
+app.use("/admin", adminRouter);
 
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
