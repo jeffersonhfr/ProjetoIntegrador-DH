f0dec710a563dadb2c3d32ffefcc5e733a4d8ca8
Alysson de Jesus Alves <alyssonja23@gmail.com>
2021-10-30 19:20:17 -0300

    feature: função que exibe a imagem do input antes de enviar ao servidor

--- server/controllers/pacotes.js
+++ server/controllers/pacotes.js
@@ -32,14 +32,19 @@ const controller = {
     res.render('adicionar-pacote', {
       title: '| Adicionar Pacote',
       pacotes: pacotes,
+      imagens: pacotes
+      ,
       usuarioLogado: req.cookies.usuario,
       usuarioAdmin: req.cookies.admin,
     });
   }
   ,
   show: (req, res, next) => {
+    let pacote = pacotes.find(u=>req.params.id == u.id);
+
     res.render('pacote', {
-      title: '| Pacote',
+      title: '|'+ pacote.nome,
+      pacote: pacote,
       usuarioLogado: req.cookies.usuario,
       usuarioAdmin: req.cookies.admin,
     });
--- server/public/src/imagens-preview.js
+++ server/public/src/imagens-preview.js
@@ -1,9 +1,12 @@
 let imgContainer = document.querySelector('.Pacote-ImgContainer');
-let imgFirst = document.querySelector('.Pacote-ImgContainer__Img-first');
+let imgFirst = document.querySelector('.Pacote-ImgContainer__Img-first.upload');
 let imgItem = document.querySelectorAll('.Pacote-ImgContainer__Img');
 let PrevSelected = imgItem[0];
+ let inputFile=document.querySelector('#upload-img');
 
+if(imgItem.length != 0){
 PrevSelected.style.border = "4px solid #3e60bf";
+}
 
 imgItem.forEach((item) => item.addEventListener('click', imgSelect));
 function imgSelect(item) {
@@ -14,4 +17,12 @@ function imgSelect(item) {
         PrevSelected.style.border = "none";
         PrevSelected = selectedImg
     }
+}
+function uploadPreview(item){
+    let reader = new FileReader();
+
+    reader.onload = function(){
+        imgFirst.src = `${reader.result}`;
+    }
+    reader.readAsDataURL(item.target.files[0]);
 }
