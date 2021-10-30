9e7cc5f5c2d979f09eafa0b9ff2314824d8d2e3b
Alysson de Jesus Alves <alyssonja23@gmail.com>
2021-10-29 21:01:53 -0300

    feature: adicinado indicador visual da imagem selecionada no imagem previews

--- server/public/src/imagens-preview.js
+++ server/public/src/imagens-preview.js
@@ -2,10 +2,16 @@ let imgContainer = document.querySelector('.Pacote-ImgContainer');
 let imgFirst = document.querySelector('.Pacote-ImgContainer__Img-first');
 let imgItem = document.querySelectorAll('.Pacote-ImgContainer__Img');
 let PrevSelected = imgItem[0];
+
+PrevSelected.style.border = "4px solid #3e60bf";
+
 imgItem.forEach((item) => item.addEventListener('click', imgSelect));
 function imgSelect(item) {
     selectedImg = item.target;
     imgFirst.src = selectedImg.getAttribute('src');
-
-    
+    selectedImg.style.border="4px solid #3e60bf";
+    if (PrevSelected != selectedImg) {
+        PrevSelected.style.border = "none";
+        PrevSelected = selectedImg
+    }
 }
