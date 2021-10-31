8dce5f916b2017b561bae068267d560944c64c51
Alysson de Jesus Alves <alyssonja23@gmail.com>
2021-10-31 02:07:41 -0300

    feature: refatorando função que exibe e seleciona imagens antes de enviar ao servidor

--- server/public/src/imagens-preview.js
+++ server/public/src/imagens-preview.js
@@ -1,4 +1,5 @@
 let imgContainer = document.querySelector('.Pacote-ImgContainer');
+let imgUl = document.querySelector('.Pacote-ImgContainer__Img-list');
 let imgFirst = document.querySelector('.Pacote-ImgContainer__Img-first.upload');
 let imgItem = document.querySelectorAll('.Pacote-ImgContainer__Img');
 let PrevSelected = imgItem[0];
@@ -8,21 +9,46 @@ if(imgItem.length != 0){
 PrevSelected.style.border = "4px solid #3e60bf";
 }
 
-imgItem.forEach((item) => item.addEventListener('click', imgSelect));
+ 
 function imgSelect(item) {
-    selectedImg = item.target;
-    imgFirst.src = selectedImg.getAttribute('src');
-    selectedImg.style.border="4px solid #3e60bf";
-    if (PrevSelected != selectedImg) {
-        PrevSelected.style.border = "none";
-        PrevSelected = selectedImg
+   
+    imgFirst.src = item.getAttribute('src');
+    
+    if (PrevSelected != item) {  
+        item.classList.add('selected');
+        PrevSelected.classList.remove('selected');
+        PrevSelected = item;
     }
+    
 }
 function uploadPreview(item){
     let reader = new FileReader();
 
     reader.onload = function(){
+       
+        let newLi = document.createElement('li');
+        let newImg = document.createElement('img');
+        
+        if(imgItem.length < 6){
+
+        
+        newImg.src = `${reader.result}`
+        
+        newImg.className = 'Pacote-ImgContainer__Img';
+        newLi.appendChild(newImg);
+        imgUl.appendChild(newLi);
+        
+        } 
         imgFirst.src = `${reader.result}`;
+        
+        newImg.classList.add('selected');        
+       if(PrevSelected !=null){
+        PrevSelected.classList.remove('selected');
+       }
+        PrevSelected = newImg;
+
+        imgUl = document.querySelector('.Pacote-ImgContainer__Img-list');
+        newImg.setAttribute('onclick','imgSelect(this)');
     }
     reader.readAsDataURL(item.target.files[0]);
 }
