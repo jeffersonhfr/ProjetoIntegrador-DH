c62a157c92336ab08ff41387338c5ee555d44058
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2021-11-10 17:26:06 -0300

    feature: add avatar default

--- server/controllers/cadastro.js
+++ server/controllers/cadastro.js
@@ -1,37 +1,44 @@
 const fs = require('fs'),
   path = require('path'),
-  bcrypt = require('bcrypt')
+  bcrypt = require('bcrypt');
 
 const controller = {
   index: (req, res, next) => {
     res.render('cadastro', {
       title: '| Cadastre-se',
       usuarioLogado: req.cookies.usuario,
-      usuarioAdmin: req.cookies.admin
+      usuarioAdmin: req.cookies.admin,
     });
   },
   cadastro: (req, res, next) => {
     res.render('cadastro', {
       title: '| Cadastre-se',
-      titulo:'Teste',
-      subtitulo:'Teste'
+      titulo: 'Teste',
+      subtitulo: 'Teste',
     });
   },
   add: (req, res, next) => {
-    const usuarios = fs.readFileSync(path.join(__dirname, '..', 'data', 'usuariosPlaceholder.json'), 'utf-8')
-    let usuariosNew = JSON.parse(usuarios)
-    let newUsuario = req.body
-    let senhaC = bcrypt.hashSync(newUsuario.senha, 10)
-    newUsuario.senha =  senhaC
-    let newId = usuariosNew[usuariosNew.length - 1].id + 1
-    newUsuario.criadoEm = new Date()
-    newUsuario.modificadoEm = new Date()
-    newUsuario.admin = false
-    newUsuario.id = newId
-    usuariosNew.push(newUsuario)
-    fs.writeFileSync(path.join(__dirname, '..', 'data', 'usuariosPlaceholder.json'), JSON.stringify(usuariosNew))
-    res.redirect('../../login')
-  }
+    const usuarios = fs.readFileSync(
+      path.join(__dirname, '..', 'data', 'usuariosPlaceholder.json'),
+      'utf-8',
+    );
+    let usuariosNew = JSON.parse(usuarios);
+    let newUsuario = req.body;
+    let senhaC = bcrypt.hashSync(newUsuario.senha, 10);
+    newUsuario.senha = senhaC;
+    let newId = usuariosNew[usuariosNew.length - 1].id + 1;
+    newUsuario.criadoEm = new Date();
+    newUsuario.modificadoEm = new Date();
+    newUsuario.admin = false;
+    newUsuario.avatar = '/assets/img/user.png';
+    newUsuario.id = newId;
+    usuariosNew.push(newUsuario);
+    fs.writeFileSync(
+      path.join(__dirname, '..', 'data', 'usuariosPlaceholder.json'),
+      JSON.stringify(usuariosNew),
+    );
+    res.redirect('../../login');
+  },
 };
 
 module.exports = controller;
