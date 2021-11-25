2cd2bab3d0ef5cba743b77fc7f04c8190d7b8e55
Anna Raphaela Berto <raphaelaberto@gmail.com>
2021-11-25 18:30:20 -0300

    feature: incluindo banco de dados na na view de editar usuario

--- server/controllers/listarUsuarios.js
+++ server/controllers/listarUsuarios.js
@@ -1,5 +1,6 @@
 const { getAllUser } = require("../services/usuarios");
-const { getUserById } = require("../services/pacotes");
+const { getUserById } = require("../services/usuarios");
+const bcrypt = require("bcrypt");
 
 const controller = {
   index: async (req, res, next) => {
@@ -17,7 +18,7 @@ const controller = {
     const { id } = req.params;
     const user = await getUserById(id);
     console.log(user);
-    res.render("usuario", {
+    res.render("usuario-edit", {
       title: "| Usuario",
       user,
       usuarioLogado: req.cookies.usuario,
@@ -28,6 +29,8 @@ const controller = {
   edit: async (req, res, next) => {
     const { id } = req.params;
     const user = await getUserById(id);
+    console.log(user);
+
     res.render("usuario-edit", {
       title: "Usuário",
       subtitulo: `Usuário #${id}`,
@@ -36,6 +39,22 @@ const controller = {
       usuarioAdmin: req.cookies.admin,
       usuarioAvatar: req.cookies.avatar,
     });
+    user.nome = req.body.nome;
+    user.email = req.body.email;
+    user.nascimento = req.body.nascimento;
+    user.telefone = req.body.telefone;
+    user.cpf = req.body.cpf;
+    user.cep = req.body.cep;
+    user.logradouro = req.body.endereco;
+    user.complemento = req.body.complemento;
+    user.localidade = req.body.cidade;
+    user.uf = req.body.uf;
+    (user.avatar = req.body.avatar), (user.modificadoEm = new Date());
+    user.push(user);
+
+    // FALTA A SENHA HASH
+
+    // res.redirect("../perfil");
   },
 };
 
