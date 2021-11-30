bb87308d4a6352c75b93a1378298728060503807
Anna Raphaela Berto <raphaelaberto@gmail.com>
2021-11-29 21:12:51 -0300

    feature: integrando a view de visualizacao do usuario ao banco de dados

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const usuarioController = require('../controllers/usuario');
+const usuarioController = require("../controllers/usuario");
 
-router.get('/', usuarioController.index);
+router.get("/", usuarioController.index);
 
-router.get('/edit', usuarioController.form_edit);
-router.put('/edit', usuarioController.edit);
+router.get("/edit", usuarioController.form_edit);
+router.put("/edit", usuarioController.edit);
 
 module.exports = router;
