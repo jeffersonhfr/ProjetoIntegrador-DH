fc9f673815340d953f60f3fe2b6e01a1dfbb2c4d
Anna Raphaela Berto <raphaelaberto@gmail.com>
2021-11-30 21:41:21 -0300

    feature: adicionando autenticacao por busca no banco de dados

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,53 +1,47 @@
-const usuariosPlaceholder = require('../data/usuariosPlaceholder.json');
-const bcrypt = require('bcrypt');
+const usuariosPlaceholder = require("../data/usuariosPlaceholder.json");
+const bcrypt = require("bcrypt");
+const { getUserByEmail } = require("../services/usuarios");
 
 const auth = async (req, res, next) => {
-  // LIMPEZA DE COOKIES
-  res.clearCookie('usuario');
-  res.clearCookie('admin');
+  try {
+    // LIMPEZA DE COOKIES
+    res.clearCookie("usuario");
+    res.clearCookie("admin");
 
-  // CAPTURA DO EMAIL E SENHA ENVIADOS
-  const { email, senha } = await req.body;
+    // CAPTURA DO EMAIL E SENHA ENVIADOS
+    const { email, senha } = await req.body;
 
-  // BUSCA POR USUÁRIO RELACIONADO AOS DADOS ENVIADOS
-  const usuarioLogado = usuariosPlaceholder.filter((usuario) => {
-    if (usuario.email === email) {
-      if (bcrypt.compareSync(senha, usuario.senha)) {
-        return usuario;
-      }
+    // BUSCA POR USUÁRIO RELACIONADO AOS DADOS ENVIADOS
+    const user = await getUserByEmail(email);
+    console.log(user);
+    if (!user) {
+      throw Error("Usuario nao encontrado!");
     }
-  });
+    if (!bcrypt.compareSync(senha, user.senha)) {
+      throw Error("Senha invalida!");
+    }
 
-  // CASO NÃO ENCONTREMOS UM USUÁRIO COM ESSES DADOS
-  if (!usuarioLogado.length) {
-    res.render('login', {
-      title: '| Login',
-      erro: 'Usuário ou senha inválida!',
+    // FILTRAMOS ALGUNS CAMPOS COM O JSON.STRINGIFY (COMO A SENHA)
+    let usuario = user;
+
+    // DEFINIMOS OS COOKIES USUÁRIO (OBJETO) E ADMIN (BOOLEANO)
+    res.cookie("usuario", usuario);
+    res.cookie("admin", `${usuario.Admin}`);
+    res.cookie("avatar", `${usuarioAvatar}`);
+
+    // CONTINUA PARA A PRÓXIMA ETAPA
+    next();
+
+    return;
+  } catch (error) {
+    console.log(error);
+    res.render("login", {
+      title: "| Login",
+      erro: "Usuário ou senha inválida!",
       usuarioLogado: req.cookies.usuario,
       usuarioAdmin: req.cookies.admin,
     });
   }
-
-  // FILTRAMOS ALGUNS CAMPOS COM O JSON.STRINGIFY (COMO A SENHA)
-  let usuario = JSON.parse(
-    JSON.stringify(usuarioLogado[0], [
-      'id',
-      'nome',
-      'email',
-      'admin',
-      'avatar',
-    ]),
-  );
-
-  // DEFINIMOS OS COOKIES USUÁRIO (OBJETO) E ADMIN (BOOLEANO)
-  res.cookie('usuario', usuario);
-  res.cookie('admin', `${usuarioLogado[0].admin}`);
-  res.cookie('avatar', `${usuarioLogado[0].avatar}`);
-
-  // CONTINUA PARA A PRÓXIMA ETAPA
-  next();
-
-  return;
 };
 
 module.exports = auth;
