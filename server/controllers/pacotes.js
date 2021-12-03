5a22c68acb4a161c6bff4b26ad8be27b61d7d882
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2021-12-03 20:01:37 -0300

    feature: mergendo branchs

--- server/controllers/pacotes.js
+++ server/controllers/pacotes.js
@@ -9,16 +9,12 @@ const {
 const { createImages } = require('../services/package_images');
 const fs = require('fs');
 
-const { createAddtionalPacote } = require('../services/adicionalPacote');
-
 const { getAllCategorias } = require('../services/categorias');
 const { getAllAddtionals } = require('../services/adicionais');
-const { getAllImages } = require('../services/package_images.js');
 
 const controller = {
   index: async (req, res, next) => {
     const pack = await getAllPackages();
-
     res.render('pacotes', {
       title: '| Pacote',
       pack,
@@ -36,10 +32,8 @@ const controller = {
   add: async (req, res, next) => {
     const categorias = await getAllCategorias();
     const adicionais = await getAllAddtionals();
-    const imagens = await getAllImages();
     console.log(categorias);
     console.log(adicionais);
-    console.log(imagens);
     res.render('adicionar-pacote', {
       title: '| Adicionar Pacote',
       categorias,
@@ -81,9 +75,7 @@ const controller = {
     }
 
     if (create) {
-      if (createImg) {
-        res.redirect('../pacotes');
-      }
+      res.redirect('../pacotes');
     } else {
       res.status(500).send('Erro ao criar seu pacote');
     }
@@ -93,7 +85,6 @@ const controller = {
     const { id } = req.params;
     const pack = await getPackagesById(id);
     console.log(pack);
-    console.log(pack[0].package_images);
     res.render('pacote', {
       title: '| Pacote',
       pack,
@@ -129,75 +120,12 @@ const controller = {
   },
   update: async (req, res, next) => {
     const { id } = req.params;
-    const pack = await getPackagesById(id);
-
-    if (req.files['capa']) {
-      console.log('0 existe');
-      var imagemCapa = `/assets/img/upload/${req.files['capa'][0].filename}`;
-    } else {
-      console.log('0 nao existe');
-      var imagemCapa = pack[0].imagemCapa;
-    }
-
-    if (req.files['imagem01']) {
-      var imagem1 = `/assets/img/upload/${req.files['imagem01'][0].filename}`;
-    } else {
-      var imagem1 = pack[0].imagem01;
-    }
-
-    if (req.files['imagem02']) {
-      var imagem2 = `/assets/img/upload/${req.files['imagem02'][0].filename}`;
-    } else {
-      var imagem2 = pack[0].imagem02;
-    }
-
-    if (req.files['imagem03']) {
-      var imagem3 = `/assets/img/upload/${req.files['imagem03'][0].filename}`;
-    } else {
-      var imagem3 = pack[0].imagem03;
-    }
-
-    if (req.files['imagem04']) {
-      var imagem4 = `/assets/img/upload/${req.files['imagem04'][0].filename}`;
-    } else {
-      var imagem4 = pack[0].imagem04;
-    }
-
-    if (req.files['imagem05']) {
-      var imagem5 = `/assets/img/upload/${req.files['imagem05'][0].filename}`;
-    } else {
-      var imagem5 = pack[0].imagem05;
-    }
-
-    if (req.files['imagem06']) {
-      var imagem6 = `/assets/img/upload/${req.files['imagem06'][0].filename}`;
-    } else {
-      var imagem6 = pack[0].imagem06;
-    }
-
-    const dataPackage = {
-      nomePacote: req.body.nomePacote,
-      nomeHotel: req.body.nomeHotel,
-      diarias: req.body.diarias,
-      preco: req.body.preco,
-      promocaoPorcentagem: req.body.promocaoPorcentagem,
-      parcelas: req.body.parcelas,
-      imagemCapa: `${imagemCapa}`,
-      imagem01: `${imagem1}`,
-      imagem02: `${imagem2}`,
-      imagem03: `${imagem3}`,
-      imagem04: `${imagem4}`,
-      imagem05: `${imagem5}`,
-      imagem06: `${imagem6}`,
-      sobre: req.body.sobre,
-      pontoTuristico: req.body.pontoTuristico,
-    };
 
     if (!id) {
       res.status(400).send('Ops... não encontramos o seu to do');
     }
 
-    const update = await updatePacote(id, dataPackage);
+    const update = await updatePacote(id, req.body);
 
     if (update) {
       res.redirect('../../pacotes');
