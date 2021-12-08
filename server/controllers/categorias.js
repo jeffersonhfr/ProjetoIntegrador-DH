4d97fbe85e0c662b9849905ad358610ed49d0199
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2021-12-08 18:58:23 -0300

    feature: ajustando view e controller categoria List

---
+++ server/controllers/categorias.js
@@ -0,0 +1,109 @@
+// const { createImages } = require('../services/package_images');
+// const fs = require('fs');
+
+const { getAllCategorias } = require('../services/categorias');
+
+const controller = {
+  index: async (req, res, next) => {
+    const categoria = await getAllCategorias();
+    console.log(categoria);
+    res.render('categoria-list', {
+      title: '| Listar Categoria',
+      categoria,
+      usuarioLogado: req.cookies.usuario,
+      usuarioAdmin: req.cookies.admin,
+      usuarioAvatar: req.cookies.avatar,
+    });
+  },
+  add: async (req, res, next) => {
+    // const categorias = await getAllCategorias();
+    // const adicionais = await getAllAddtionals();
+    // console.log(categorias);
+    // console.log(adicionais);
+    // res.render('adicionar-pacote', {
+    //   title: '| Adicionar Pacote',
+    //   categorias,
+    //   adicionais,
+    //   valor: (valor) => {
+    //     return valor.toLocaleString('pt-BR', {
+    //       style: 'currency',
+    //       currency: 'BRL',
+    //     });
+    //   },
+    //   usuarioLogado: req.cookies.usuario,
+    //   usuarioAdmin: req.cookies.admin,
+    //   usuarioAvatar: req.cookies.avatar,
+    // });
+  },
+  create: async (req, res, next) => {
+    // if (req.files) {
+    //   pacote = {};
+    //   pacote.nomePacote = req.body.nomePacote;
+    //   pacote.nomeHotel = req.body.nomeHotel;
+    //   pacote.diarias = req.body.diarias;
+    //   pacote.passagemAerea = req.body.passagemAerea;
+    //   pacote.nacional = req.body.nacional;
+    //   pacote.preco = req.body.preco;
+    //   pacote.promocaoPorcentagem = req.body.promocaoPorcentagem;
+    //   pacote.parcelas = req.body.parcelas;
+    //   pacote.package_images = req.files;
+    //   const create = await createPacote(pacote);
+    //   const createImg = await pacote.package_images.forEach((element) =>
+    //     createImages({
+    //       packageId: create.id,
+    //       src: '/assets/img/package/' + element.filename,
+    //     }),
+    //   );
+    //   return createImg;
+    // }
+    // if (create) {
+    //   res.redirect('../pacotes');
+    // } else {
+    //   res.status(500).send('Erro ao criar seu pacote');
+    // }
+  },
+  edit: async (req, res, next) => {
+    // const { id } = req.params;
+    // const pack = await getPackagesById(id);
+    // const categorias = await getAllCategorias();
+    // const adicionais = await getAllAddtionals();
+    // console.log(pack);
+    // console.log(categorias);
+    // console.log(adicionais);
+    // res.render('pacote-edit', {
+    //   title: '| Pacote',
+    //   pack,
+    //   categorias,
+    //   adicionais,
+    //   usuarioLogado: req.cookies.usuario,
+    //   usuarioAdmin: req.cookies.admin,
+    //   usuarioAvatar: req.cookies.avatar,
+    // });
+  },
+  update: async (req, res, next) => {
+    // const { id } = req.params;
+    // if (!id) {
+    //   res.status(400).send('Ops... não encontramos o seu to do');
+    // }
+    // const update = await updatePacote(id, req.body);
+    // if (update) {
+    //   res.redirect('../../pacotes');
+    // } else {
+    //   res.status(500).send('Ops... deu ruim...');
+    // }
+  },
+  delete: (req, res, next) => {
+    // res.redirect('../../pacotes');
+  },
+  destroy: async (req, res, next) => {
+    // const { id } = req.params;
+    // const destroy = await destroyPacote(id);
+    // if (destroy) {
+    //   res.redirect('../../pacotes');
+    // } else {
+    //   res.status(500).send('Ops... deu ruim...');
+    // }
+  },
+};
+
+module.exports = controller;
---
+++ server/routes/listarCategoria.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var listarCategoriaController = require('../controllers/categorias');
+const adminMiddleware = require('../middlewares/admin');
+
+// const multer = require('multer');
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './public/assets/img/upload');
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.fieldname + '-' + Date.now() + '.jpg');
+//   },
+// });
+// const upload = multer({ storage: storage });
+
+// const uploadImage = require('../middlewares/uploadImage');
+
+/* GET home page. */
+router.get('/', listarCategoriaController.index);
+
+router.get('/adicionar', adminMiddleware, listarCategoriaController.add);
+router.post('/adicionar', adminMiddleware, listarCategoriaController.create);
+
+router.get('/:id/editar', adminMiddleware, listarCategoriaController.edit);
+router.post('/:id/editar', adminMiddleware, listarCategoriaController.update);
+
+router.post('/:id/delete', adminMiddleware, listarCategoriaController.destroy);
+
+module.exports = router;
