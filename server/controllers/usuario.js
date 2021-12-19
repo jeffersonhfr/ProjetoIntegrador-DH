451d085964841f086788d01a13c8430d02dedebb
Anna Raphaela Berto <raphaelaberto@gmail.com>
2021-12-19 09:46:42 -0300

    fix: imagem do avatar nao funcionando

--- server/controllers/usuario.js
+++ server/controllers/usuario.js
@@ -22,43 +22,41 @@ const controller = {
     }
   },
 
-  // show: async (req, res, next) => {
-  //   const { id } = req.params;
-  //   const user = await getUserById(id);
-  //   console.log(user);
-  //   res.render("usuario-edit", {
-  //     title: "| Usuario",
-  //     user,
-  //     usuarioLogado: req.cookies.usuario,
-  //     usuarioAdmin: req.cookies.admin,
-  //     usuarioAvatar: req.cookies.avatar,
-  //   });
-  // },
-
   edit: async (req, res, next) => {
-    const user = req.body;
-    user.senha = bcrypt.hashSync(req.body.senha, 10);
     const { id } = req.cookies.usuario;
-    const update = await updateUser(id, req.body, user);
-    // console.log(user);
-    let usuarioLogado = req.cookies.usuario;
-    let usuarioAdmin = req.cookies.admin;
-    res.render("usuario-edit", {
-      title: "Usuário",
-      subtitulo: `Usuário #${id}`,
-      usuarioLogado: usuarioLogado,
-      usuarioAdmin: usuarioAdmin,
-      usuarioAvatar: req.cookies.avatar,
-    });
+
+    if (req.file) {
+      var usuarioAvatar = `${req.file.filename}`;
+    } else {
+      usuarioAvatar = req.cookies.avatar;
+    }
+
+    const user = {
+      userNome: req.body.nome,
+      userEmail: req.body.email,
+      // userSenha: bcrypt.hashSync(req.body.senha, 10),
+      userNascimento: req.body.nascimento,
+      userTelefone: req.body.telefone,
+      userCpf: req.body.cpf,
+      userCep: req.body.cep,
+      userLogradouro: req.body.endereco,
+      userComplemento: req.body.complemento,
+      userLocalidade: req.body.cidade,
+      userUf: req.body.uf,
+      userAvatar: usuarioAvatar,
+      userModificadoEm: new Date(),
+    };
+
+    console.log(usuarioAvatar);
+    console.log(req.file.filename);
+
+    const update = await updateUser(id, user);
+
     if (update) {
       res.redirect("../perfil");
     } else {
       res.status(500).send("Ops... deu ruim...");
     }
-
-    // FALTA A SENHA HASH?
-
-    // res.redirect("../perfil");
   },
 
   form_edit: async (req, res, next) => {
