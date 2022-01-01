c9a3e312ebbeee3e17d967993cef9fb53257917f
Alysson de Jesus Alves <alyssonja23@gmail.com>
2022-01-01 02:18:38 -0300

    feature: adaptando o header para as demais paginas

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,202 +3,211 @@ import menuToggle from './scripts/menu'
 const Header = () => {
   const [isLogado, setIsLogado] = useState(true);
   const [isAdmin, setIsAdmin] = useState(true);
+  const [isIndex,setIsIndex] = useState(true);
   
   let perfil = '/assets/img/Aventura.jpg'
-  
+  let header = (<header class="cabecalho">
 
-  return (
-    <header class="cabecalho">
-      <div class="cabecalho__sobreposicao">
-        <div class="container cabecalho__navegacao">
-          <a href="/">
-            <div>
-              <img
-                alt="Logo Viagem.com"
-                class="cabecalho_logo"
-                src="/assets/img/logo01.png"
-                alt="logo"
-                width="234px"
-                height="51px"
-              />
-            </div>
-          </a>
-          <nav class="cabecalho__menu-mobile">
-            <input
-              class="cabecallho__menu-hamburguer"
-              id="cabecallho__menu-hamburguer"
-              type="checkbox"
-            />
-            <label for="cabecallho__menu-hamburguer">
-              <div class="menu-hamburguer__conteudo">
-                <span class="menu-hamburguer__icone"></span>
-                <div class="menu-hamburguer__menu-mobile">
-                  <ul>
-                    <li>
-                      <a href="/pacotes">Viagens Temáticas</a>
-                    </li>
-                    <li>
-                      <a href="/pacotes">Nossos Pacotes</a>
-                    </li>
-                    <li>
-                      <a href="/sobre">Sobre Nós</a>
-                    </li>
-                    <li>
-                      <a href="/ajuda">Ajuda</a>
-                    </li>
-
-                    {isLogado && !isAdmin ? (
-                      <>
-                        <li class="separador"></li>
-                        <li>
-                          <a href="/perfil">Meu Perfil</a>
-                        </li>
-                        <li>
-                          <a href="/minhas-viagens">Minhas Viagens</a>
-                        </li>
-                        <li class="login">
-                          <a href="/logout">Logout</a>
-                        </li>
-                      </>
-                    ) : isLogado && isAdmin ? (
-                      <>
-                        <li class="separador"></li>
-                        <li>
-                          <a href="/listarUsuarios">Gestão de Usuários</a>
-                        </li>
-                        <li>
-                          <a href="/minhas-viagens/todos-pedidos">
-                            Visualizar Todos os Pedidos
-                          </a>
-                        </li>
-                        <li>
-                          <a href="/listarCategoria">Cadastrar Categorias</a>
-                        </li>
-                        <li>
-                          <a href="/listarAdicional">Cadastrar Adicionais</a>
-                        </li>
-                        <li class="login">
-                          <a href="/logout">Logout</a>
-                        </li>
-                      </>
-                    ) : (
-                      <li class="login">
-                        <a href="/login">Login</a>
-                      </li>
-                    )}
-                  </ul>
-                </div>
-              </div>
-            </label>
-          </nav>
-
-          <nav class="cabecalho__menu-desk">
-            <ul class="flex">
-              <li>
-                <a href="/pacotes">Viagens Temáticas</a>
-              </li>
-              <li>
-                <a href="/pacotes">Nossos Pacotes</a>
-              </li>
-              <li>
-                <a href="/sobre">Sobre Nós</a>
-              </li>
-              <li>
-                <a href="/ajuda">Ajuda</a>
-              </li>
-
-              {isLogado && !isAdmin ? (
-                <>
-                  <li class="menu__desk" onClick={menuToggle}>
-                    {' '}
-                    <img
-                      alt="Foto do Usuário"
-                      class="menu__desk-foto"
-                      src={perfil}
-                      alt="foto de perfil"
-                      width="50px"
-                      height="50px"
-                    />
-                  </li>
-                </>
-              ) : isLogado && isAdmin ? (
-                <>
-                  <li class="menu__desk" onClick={menuToggle}>
-                    {' '}
-                    <img
-                      alt="Foto do Usuário"
-                      class="menu__desk-foto"
-                      src={perfil}
-                      alt="foto de perfil"
-                    />
-                  </li>
-                </>
-              ) : (
-                <li>
-                  <a href="/login">Login</a>
-                </li>
-              )}
-            </ul>
-          </nav>
+    <div class="container cabecalho__navegacao">
+      <a href="/">
+        <div>
+          <img
+            alt="Logo Viagem.com"
+            class="cabecalho_logo"
+            src="/assets/img/logo01.png"
+            alt="logo"
+            width="234px"
+            height="51px"
+          />
         </div>
+      </a>
+      <nav class="cabecalho__menu-mobile">
+        <input
+          class="cabecallho__menu-hamburguer"
+          id="cabecallho__menu-hamburguer"
+          type="checkbox"
+        />
+        <label for="cabecallho__menu-hamburguer">
+          <div class="menu-hamburguer__conteudo">
+            <span class="menu-hamburguer__icone"></span>
+            <div class="menu-hamburguer__menu-mobile">
+              <ul>
+                <li>
+                  <a href="/pacotes">Viagens Temáticas</a>
+                </li>
+                <li>
+                  <a href="/pacotes">Nossos Pacotes</a>
+                </li>
+                <li>
+                  <a href="/sobre">Sobre Nós</a>
+                </li>
+                <li>
+                  <a href="/ajuda">Ajuda</a>
+                </li>
 
-        {isLogado && !isAdmin ? (
-          <>
-            <div class="container flex-end">
-              <div class="menu__desk-conteudo">
-                <ul>
-                  <li>
-                    <a href="/perfil">Meu Perfil</a>
-                  </li>
-                  <li>
-                    <a href="/minhas-viagens">Minhas Viagens</a>
-                  </li>
+                {isLogado && !isAdmin ? (
+                  <>
+                    <li class="separador"></li>
+                    <li>
+                      <a href="/perfil">Meu Perfil</a>
+                    </li>
+                    <li>
+                      <a href="/minhas-viagens">Minhas Viagens</a>
+                    </li>
+                    <li class="login">
+                      <a href="/logout">Logout</a>
+                    </li>
+                  </>
+                ) : isLogado && isAdmin ? (
+                  <>
+                    <li class="separador"></li>
+                    <li>
+                      <a href="/listarUsuarios">Gestão de Usuários</a>
+                    </li>
+                    <li>
+                      <a href="/minhas-viagens/todos-pedidos">
+                        Visualizar Todos os Pedidos
+                      </a>
+                    </li>
+                    <li>
+                      <a href="/listarCategoria">Cadastrar Categorias</a>
+                    </li>
+                    <li>
+                      <a href="/listarAdicional">Cadastrar Adicionais</a>
+                    </li>
+                    <li class="login">
+                      <a href="/logout">Logout</a>
+                    </li>
+                  </>
+                ) : (
                   <li class="login">
-                    <a href="/logout">Logout</a>
+                    <a href="/login">Login</a>
                   </li>
-                </ul>
-              </div>
+                )}
+              </ul>
             </div>
-          </>
-        ) : isLogado && isAdmin ? (
-          <>
-            <div class="container flex-end">
-              <div class="menu__desk-conteudo">
-                <ul>
-                  <li>
-                    <a href="/perfil">Meu Perfil</a>
-                  </li>
-                  <li>
-                    <a href="/minhas-viagens">Minhas Viagens</a>
-                  </li>
-                  <li class="separador"></li>
-                  <li>
-                    <a href="/listarUsuarios">Gestão de Usuários</a>
-                  </li>
-                  <li>
-                    <a href="/minhas-viagens/todos-pedidos">
-                      Visualizar Todos os Pedidos
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/listarCategoria">Cadastrar Categorias</a>
-                  </li>
-                  <li>
-                    <a href="/listarAdicional">Cadastrar Adicionais</a>
-                  </li>
-                  <li class="login">
-                    <a href="/logout">Logout</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </>
-        ) : null}
+          </div>
+        </label>
+      </nav>
 
-      
-      </div>
-    </header>
-  );
-};
+      <nav class="cabecalho__menu-desk">
+        <ul class="flex">
+          <li>
+            <a href="/pacotes">Viagens Temáticas</a>
+          </li>
+          <li>
+            <a href="/pacotes">Nossos Pacotes</a>
+          </li>
+          <li>
+            <a href="/sobre">Sobre Nós</a>
+          </li>
+          <li>
+            <a href="/ajuda">Ajuda</a>
+          </li>
+
+          {isLogado && !isAdmin ? (
+            <>
+              <li class="menu__desk" onClick={menuToggle}>
+                {' '}
+                <img
+                  alt="Foto do Usuário"
+                  class="menu__desk-foto"
+                  src={perfil}
+                  alt="foto de perfil"
+                  width="50px"
+                  height="50px"
+                />
+              </li>
+            </>
+          ) : isLogado && isAdmin ? (
+            <>
+              <li class="menu__desk" onClick={menuToggle}>
+                {' '}
+                <img
+                  alt="Foto do Usuário"
+                  class="menu__desk-foto"
+                  src={perfil}
+                  alt="foto de perfil"
+                />
+              </li>
+            </>
+          ) : (
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </div>
+
+    {isLogado && !isAdmin ? (
+      <>
+        <div class="container flex-end">
+          <div class="menu__desk-conteudo">
+            <ul>
+              <li>
+                <a href="/perfil">Meu Perfil</a>
+              </li>
+              <li>
+                <a href="/minhas-viagens">Minhas Viagens</a>
+              
+    
+              </li>
+              <li class="login">
+                <a href="/logout">Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </>
+    ) : isLogado && isAdmin ? ( 
+      <>
+        <div class="container flex-end">
+          <div class="menu__desk-conteudo">
+            <ul>
+              <li>
+                <a href="/perfil">Meu Perfil</a>
+              </li>
+              <li>
+                <a href="/minhas-viagens">Minhas Viagens</a>
+              </li>
+              <li class="separador"></li>
+              <li>
+                <a href="/listarUsuarios">Gestão de Usuários</a>
+              </li>
+              <li>
+                <a href="/minhas-viagens/todos-pedidos">
+                  Visualizar Todos os Pedidos
+                </a>
+              </li>
+              <li>
+                <a href="/listarCategoria">Cadastrar Categorias</a>
+              </li>
+              <li>
+                <a href="/listarAdicional">Cadastrar Adicionais</a>
+              </li>
+              <li class="login">
+                <a href="/logout">Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </>
+    ) : null}
+ 
+</header>)
+
+  return (<>
+    {isIndex ? (  <div class="cabecalho__sobreposicao">
+      {header}</div>):(<>
+      <header class="cabecalho-paginas">
+        <div class="header_container">
+    
+        {header}
+
+        </div>
+      </header></>)}
+</>);
+}
 
 export default Header;
--- client/src/components/Header/scripts/menu.js
+++ client/src/components/Header/scripts/menu.js
@@ -3,11 +3,6 @@ const menuToggle = (e)=>{
 
     let menuUser = document.querySelector('.menu__desk-conteudo');
 
-
-    // iconUser.addEventListener('click',function() {
-    //   menuUser.classList.toggle('mostrar-menu');
-    // })
-
         menuUser.classList.toggle('mostrar-menu');
    
 }
