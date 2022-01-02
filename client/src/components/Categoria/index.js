747cc4b390e4e43485189f0d647a02e39413c872
Alysson de Jesus Alves <alyssonja23@gmail.com>
2022-01-01 22:27:15 -0300

    feature: importando imagens para categoria

--- client/src/components/Categoria/index.js
+++ client/src/components/Categoria/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 const Categoria = ({categoria}) =>{
-
+  
+  ///  import imagemCategoria from `../../../public/assets/img/categoria/${categoria.imagemCategoria}`
 return <>
     
     { 
         <>
             <li class="Tema">
-                {<div class="Container Img" style={{backgroundImage: `url(/assets/img/categoria/${categoria.imagemCategoria });`}}>
+                {<div class="Container Img" style={{backgroundImage: `url(/assets/img/categoria/${categoria.imagemCategoria})`}}>
                     <a href={`/pacotes/?categoria=${categoria.nomeCategoria}`} style={{backgroundColor :`${categoria.corCategoria}aa`}}>
                          <h1>
                             {categoria.nomeCategoria}
