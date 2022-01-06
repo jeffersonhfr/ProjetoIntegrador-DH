eeac8db38bc884a402f1a135c6810e3c8634d39c
Anna Raphaela Berto <raphaelaberto@gmail.com>
2022-01-05 21:18:58 -0300

    feature: adicionando componente de barra de busca em react

---
+++ client/src/components/BarraDeBusca/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const BarraDeBusca = () => {
+  return (
+    <div className="container" style={{ justifyContent: "center" }}>
+      <section className="container-busca">
+        <form className="container-busca__form" action="/pesquisa">
+          <div className="container-busca__form__campos">
+            <div className="container-busca__form__campos__itens container-busca__form__campos__itens--primeiro">
+              <div className="busca__campo">
+                <label for="onde">Para onde desejar viajar?</label>
+                <input
+                  type="text"
+                  id="onde"
+                  name="onde"
+                  placeholder="Escreva aqui o local para onde deseja ir"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <input
+            type="submit"
+            value="Buscar"
+            className="busca__campo__submit"
+          />
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default BarraDeBusca;
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -3,11 +3,13 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Categories from "../../components/Categories";
 import NewsLetter from "../../components/NewsLetter";
+import BarraDeBusca from "../../components/BarraDeBusca";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <BarraDeBusca />
       <Categories />
       <NewsLetter />
       <Footer />
