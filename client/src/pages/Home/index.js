067711634ca21e977801d767425bf30b46e5335d
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2022-01-10 19:07:59 -0300

    bugfix: Ajustando overlay HEADER

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import Categorias from "../../components/Categorias";
-import NewsLetter from "../../components/NewsLetter";
-import BarraDeBusca from "../../components/BarraDeBusca";
+import React from 'react';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import Categorias from '../../components/Categorias';
+import NewsLetter from '../../components/NewsLetter';
+import BarraDeBusca from '../../components/BarraDeBusca';
 
 const Home = () => {
   return (
