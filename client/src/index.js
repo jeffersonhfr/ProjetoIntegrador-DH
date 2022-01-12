6c4306d27473649454281489082710a1396ebaa8
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2022-01-12 11:04:05 -0300

    feature: add sistema de rotas

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import Home from "./pages/Home/index"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home/>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
