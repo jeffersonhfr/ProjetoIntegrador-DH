557ea8ebc68f3d09caef9f20b8ffab2d477bbd77
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2022-01-12 12:27:41 -0300

    feature: Pagina 404

--- client/src/pages/404/index.js
+++ client/src/pages/404/index.js
@@ -6,9 +6,18 @@ const NotFound = () => {
   return (
     <>
       <Header />
-      <main class="status404">
-        <h1>Desculpa! Página não encontrada</h1>
-      </main>
+      <div class="container container-historico container-historico-null">
+        <div class="box1">
+          <h1>Desculpa! Página não encontrada</h1>
+          <h2>
+            Mas você pode continuar navegando. Com certeza você irá achar os
+            melhores pacotes e planejar agora mesmo suas férias?
+          </h2>
+        </div>
+        <div class="box2">
+          <img src="assets/img/404.png" width="100%" alt="" />
+        </div>
+      </div>
       <Footer />
     </>
   );
