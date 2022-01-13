d928ba3e376f729db0a8f30d76fc215872af8477
unknown <alyssonja23@gmail.com>
2022-01-13 06:58:35 -0300

    feature: adaptando ver-senha da página login

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,49 +1,49 @@
 import React from 'react';
-
+import verSenha from "./scripts/ver-senha"
 // import { Container } from './styles';
 
 const Login = ({erro})=>{
   return <>
   
-  <section class="container-login">
-      <div class="container-imagem-login">
-        <img class="imagem-login" src="/assets/img/imagem-login.jpeg" alt="imagem login" width="310px" height="290px"/>
+  <section className="container-login">
+      <div className="container-imagem-login">
+        <img className="imagem-login" src="/assets/img/imagem-login.jpeg" alt="imagem login" width="310px" height="290px"/>
       </div>
 
-      <div class="container-formulario-login">
+      <div className="container-formulario-login">
         <form action="/login" id="formulario-login" method="POST">
             {erro != null ?
-          <div class="container-formulario-login__erro">
+          <div className="container-formulario-login__erro">
             {erro}
           </div>:null}
           <fieldset>
-            <div class="formulario-login__dados">
+            <div className="formulario-login__dados">
               <input type="email" id="email" name="email" placeholder="Insira seu e-mail" required/>
-              <i class="fa fa-user" aria-hidden="true"></i>
+              <i className="fa fa-user" aria-hidden="true"></i>
             </div>
-            <div class="formulario-login__dados">
+            <div className="formulario-login__dados">
 
-              <input type="password" class="login__senha" id="senha" name="senha" placeholder="Senha" required placeholder="Senha" required/>
-              <i class="bi bi-eye-slash" id="togglePassword"></i>
+              <input type="password" className="login__senha" id="senha" name="senha" placeholder="Senha" required placeholder="Senha" required/>
+              <i className="bi bi-eye-slash" id="togglePassword" onClick={verSenha}></i>
             
             </div>
           </fieldset>
 
-          <div class="container-recuperar-senha">
+          <div className="container-recuperar-senha">
             <a href="/recuperarSenha">Esqueceu sua senha?</a>
           </div>
-         {origin?
+            {/*origem?
             <input type="text" name="origem" id="origem" value="<%= origem %>" hidden/>
             :null
-           }
-              <div class="container-botao-login">
-                <button class="botao-login" type="submit">ENTRAR</button>
+           */}
+              <div className="container-botao-login">
+                <button className="botao-login" type="submit">ENTRAR</button>
               </div>
-              <div class="container-cadastro">
+              <div className="container-cadastro">
                 <p>Ainda não é cadastrado? </p>
               </div>
               <div>
-                <p class="cadastro__link-cadastro"><a href="/cadastro">Cadastre-se</a></p>
+                <p className="cadastro__link-cadastro"><a href="/cadastro">Cadastre-se</a></p>
               </div>
         </form>
       </div>
