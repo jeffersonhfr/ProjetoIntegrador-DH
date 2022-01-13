6a93412ca2bd843a137bb33cd7241c435e8e143e
unknown <alyssonja23@gmail.com>
2022-01-13 19:28:33 -0300

    feature: verificação de check adicionais e categorias no editar pacotes

--- client/src/pages/Pacote/index.js
+++ client/src/pages/Pacote/index.js
@@ -104,6 +104,8 @@ const Pacote = () => {
       ],
     },
   ];
+
+  
   return (
     <>
       
--- client/src/pages/PacoteEdit/index.js
+++ client/src/pages/PacoteEdit/index.js
@@ -18,6 +18,7 @@ const PacoteEdit =()=> {
         pontoTuristico:
           'Para quem procura por experiências marcantes e inesquecíveis durante as férias uma  viagem para o Egito é a resposta. O país localizado no nordeste da África possui uma história riquíssima e monumentos grandiosos – não à toa está presente em quase todos os livros de história.O Egito fica a mais de 10 mil quilômetros de distância do Brasil, os voos saindo de São Paulo costumam ter pelo menos uma escala e a viagem mais rápida demora cerca de 17h 30m. Como a conexão é feita em cidades como Istambul e Dubai, muitos turistas aproveitam para conhecê-las também.No país africano as principais cidades a serem visitadas são a capital Cairo, Luxor, Aswan e Sharm el Sheikh. Cada uma delas possui características particulares e pontos turísticos de visita obrigatória que vão além de templos e sarcófagos. Conheça alguns deles. Pirâmides de Gizé (Cairo) Essa é o ponto turístico mais conhecido do Cairo e de todo o Egito. Um dos mais antigos monumentos, o conjunto Pirâmides de Gizé fica a 18 km da capital e o acesso é fácil. Agências de turismo oferecem passeios diurnos e noturnos, esse último inclui um espetáculo de luzes e som. As principais pirâmides em que faraós foram mumificados e sepultados são as de Quéops, Quéfren e Miquerinos. A primeira é a maior delas, com 140 metros de altura e 230 metros de base. Além delas, a Grande Esfinge chama atenção. Khan el-Khalili Bazaar (Cairo) A antiga área comercial fica no coração de Cairo e é um grande bazar a céu aberto que reúne cafés, restaurantes e lojas de especiarias, joias, tecidos, artesanatos, perfumes, alimentos, entre outros. Além de ser uma forma de mergulhar na cultura local, visitar as ruelas do Khan el-Khalili Bazaar também é viver a história, já que o local, fundado no século XIV, transformou Cairo em um centro importante do comércio ao permitir comerciantes estrangeiros exporem suas mercadorias. O Khan el-Khalili Bazaar está incluso em vários pacotes de turismo oferecidos por agências – inclusive, essa é a melhor forma de conhecer o local sem se perder. O pacote da Memphis Tours, por exemplo, disponibiliza guias que falam a língua portuguesa, o que torna o passeio ainda mais proveitoso.',
         categoria:[{
+          id:1,
           nomeCategoria: 'Neve',
           corCategoria: '#d2d2d2',
           imagemCategoria: 'Neve.jpg',
@@ -25,24 +26,24 @@ const PacoteEdit =()=> {
           updatedAt: new Date(),
         }],
         adicional:[{
-          Id:1,
+          id:1,
           nomeAdicional: 'City Tour',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         { 
-         Id:2,
+         id:2,
           nomeAdicional: 'Hotel com Piscina Aquecida',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
-        { Id:3,
+        { id:3,
           nomeAdicional: 'Jantar no Palácio Real',
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
-          Id:4,
+          id:4,
           nomeAdicional: 'Entradas para Museus',
           createdAt: new Date(),
           updatedAt: new Date(),
@@ -99,6 +100,118 @@ const PacoteEdit =()=> {
         createdAt: new Date(),
         updatedAt: new Date(),
       }
+    const categorias =  [
+      {
+        id:1,
+        nomeCategoria: 'Neve',
+        corCategoria: '#d2d2d2',
+        imagemCategoria: 'Neve.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id:2,
+        nomeCategoria: 'Praia',
+        corCategoria: '#25abbd',
+        imagemCategoria: 'Praia.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id:3,
+        nomeCategoria: 'Aventura',
+        corCategoria: '#69C862',
+        imagemCategoria: 'Aventura.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id:4,
+        nomeCategoria: 'Romance',
+        corCategoria: '#c42e92',
+        imagemCategoria: 'Romantico.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id:5,
+        nomeCategoria: 'Família',
+        corCategoria: '#5b4a70',
+        imagemCategoria: 'familia.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id:6,
+        nomeCategoria: 'Histórico',
+        corCategoria: '#896d15',
+        imagemCategoria: 'historico.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ] 
+
+    const  adicionais=  [
+        { 
+          id:1,
+          nomeAdicional: 'City Tour',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        { 
+          id : 2, 
+          nomeAdicional: 'Hotel com Piscina Aquecida',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        { 
+          id : 3, 
+          nomeAdicional: 'Jantar no Palácio Real',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id : 4, 
+          nomeAdicional: 'Entradas para Museus',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id : 5, 
+          nomeAdicional: 'Ingresso para os Parques',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nomeAdicional: 'Passeio de Camelo',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id : 6, 
+          nomeAdicional: 'Aulas de Surf',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        { 
+          id : 7, 
+          nomeAdicional: 'Wi-fi Grátis',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id : 8, 
+          nomeAdicional: 'Café da Manhã',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id : 9, 
+          nomeAdicional: 'Refeição Completa',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ]
 
 
 
@@ -198,9 +311,14 @@ let loadFile=(e)=>{console.log(e)};
           <li className="Pacote-Propriedades__Item Pacote-Propriedades__Item--addpack">
             <label htmlFor="adicionais">Adicionais</label>
             <div className="checkbox-div">
-              {pacote.adicional.map((e)=>{
-                <div className="checkbox-opt">
-                  <input type="checkbox" name="adicionais" id="adicionais" readOnly value={e.id} readOnly />
+              {adicionais.map((e)=>{
+                return <div className="checkbox-opt">{
+
+                    pacote.adicional.find((i)=> i.id === e.id)
+                    ?
+                  <input type="checkbox" name="adicionais" id="adicionais" readOnly value={e.id} readOnly 
+                  defaultChecked/>:<input type="checkbox" name="adicionais" id="adicionais" readOnly value={e.id} readOnly 
+                  />}
                   {e.nomeAdicional}
                 </div>
               })}
@@ -211,9 +329,12 @@ let loadFile=(e)=>{console.log(e)};
           <li className="Pacote-Propriedades__Item Pacote-Propriedades__Item--addpack">
             <label htmlFor="categorias">Categorias</label>
             <div className="checkbox-div">
-              {pacote.categoria.map((e)=>{
-                <div className="checkbox-opt">
-                  <input type="checkbox" name="categorias" id="categorias" readOnly value={e.id}/>
+              {categorias.map((e)=>{
+                return<div className="checkbox-opt"> {
+                  pacote.categoria.find((i)=> i.id === e.id)
+                  ?
+                  <input type="checkbox" name="categorias" id="categorias" readOnly value={e.id} defaultChecked/>
+                  :<input type="checkbox" name="categorias" id="categorias" readOnly value={e.id}/>}
                   {e.nomeCategoria}
                 </div>
               })
