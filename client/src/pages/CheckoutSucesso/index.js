9e9f14ad0636cf8d477af0854695c590611bbdc7
Raphaela Berto <anna.berto@loggicom>
2022-01-15 23:29:20 -0300

    chore: adicionando pagina  de checkout

---
+++ client/src/pages/CheckoutSucesso/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Footer from '../../components/Footer';
+
+
+const CheckoutSucesso = () => {
+
+    return <>
+
+
+    <main className="container container-checkout">
+        <div className="titulo-checkout">
+            <h1>Minha Compra</h1>
+        </div>
+        <div className="pagamento__box">
+            <div className="pagamento__box__content">
+            <div className="pagamento__box__imagem">
+                <img src="<%= imagem %>" alt="" width="100%"/>
+            </div>
+            <div className="pagamento__box__detalhes pagamento__box__sucesso">
+                <ul>
+                    <li>
+                        <h2 className="pagamento__box__detalhes__titulo">Compra realizada com sucesso!</h2>
+                    </li>
+                    <li>
+                        <p className="pagamento__box__detalhes__paragrafo">Para maiores informações acesse o painel minhas viagens</p>
+                    </li>
+                    {/* if(locals.pagamento) {  */}
+                        <br/>
+                        <li>
+                        <a href="#"><span className="pagamento-link">Clique aqui</span></a>para imprimir o seu boleto
+                        </li>
+                </ul>
+            </div>
+            </div>
+
+        </div>
+        </main>
+
+        <Footer/>
+    
+     </>
+}
+    
+
+export default CheckoutSucesso;
