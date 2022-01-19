66d3d22aa19477fd55f5c014831a159e960548f7
Jefferson Rodrigues <jeffersonhfr@gmail.com>
2022-01-19 15:42:41 -0300

    bugfix: tirando elemento header e footer da index

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 import Categorias from '../../components/Categorias';
 import NewsLetter from '../../components/NewsLetter';
 import BarraDeBusca from '../../components/BarraDeBusca';
