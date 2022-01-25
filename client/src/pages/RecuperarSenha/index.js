078491f9c611debbd42738878d0c047847c45e03
Anna Raphaela Berto <raphaelaberto@gmail.com>
2022-01-25 11:47:15 -0300

    feature: adicionando pagina de recuperacao de senha

---
+++ client/src/pages/RecuperarSenha/index.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+
+const RecuperarSenha = () => {
+  let img = "../assets/img/imagem-login.jpeg";
+  return (
+    <>
+      <section class="container-login">
+        <div class="container-imagem-login">
+          <img
+            class="imagem-login"
+            src={img}
+            alt="imagem login"
+            width="310px"
+            height="290px"
+          />
+        </div>
+
+        <div class="container-formulario-login">
+          <form action="/login" id="formulario-login" method="POST">
+            <div class="container-formulario-login__erro">
+              {/* <%= erro %> */}
+            </div>
+            <fieldset>
+              <div class="formulario-login__dados-recuperar">
+                <div>
+                  <p class="redefinir-senha"> Redefinir senha </p>
+                  <p class="redefinir-senha-p">
+                    Insira abaixo o e-mail vinculado à sua conta e iremos enviar
+                    um link para redefinição de sua senha.
+                  </p>
+                </div>
+                <div class="redefinir-senha__email">
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </div>
+              </div>
+            </fieldset>
+            <div class="container-botao-login">
+              {/* <button class="botao-login" type="submit" onclick=redireciona()>Enviar</button> */}
+            </div>
+            <div class="container-cadastro">
+              <p>Ainda não é cadastrado? </p>
+            </div>
+            <div>
+              <p class="cadastro__link-cadastro">
+                <a href="/cadastro">Cadastre-se</a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
+export default RecuperarSenha;
