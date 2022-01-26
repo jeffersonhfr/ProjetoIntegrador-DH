1a6a7c4b75530567186c507aeb4fe5af90175b4b
Anna Raphaela Berto <raphaelaberto@gmail.com>
2022-01-26 19:22:59 -0300

    feature: adicionando useEfect ao componente usuario card

--- client/src/components/UsuarioCard/index.js
+++ client/src/components/UsuarioCard/index.js
@@ -1,63 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const UsuarioCard = ({ usuario }) => {
   const isLogado = true;
   const isAdmin = true;
+  const [user, setUser] = useState([]);
+
+  // const { id } = useParams();
+  // const apiURL = "http://localhost:3333/visualizarUsuario/:" + id;
+  // useEffect(() => {
+  //   fetch(apiURL)
+  //     .then((res) => res.json())
+  //     .then((res) =>
+  //       setTimeout(() => {
+  //         setUser(res.user);
+  //       })
+  //     );
+  // }, []);
+  useEffect(() => {
+    fetch("http://localhost:3333/listarUsuarios")
+      .then((res) => res.json())
+      .then((res) => setUser(res.user));
+  }, []);
+
   return (
     <>
-      <div className="travel-card travel-card__content">
-        <div className="travel-card__imagem-box">
-          <img
-            alt="Avatar Usuário"
-            src={usuario.avatar}
-            className="travel-card__imagem"
-          />
-        </div>
-        <div className="travel-card__info-box">
-          <div className="travel-card__infoDesc">
-            <ul className="Propriedades">
-              <li className="usuario-card__propriedade">Nome</li>
-              <li className="usuario-card__dado">{usuario.nome}</li>
-              <li className="usuario-card__propriedade">Email </li>
-              <li className="usuario-card__dado">{usuario.email}</li>
-            </ul>
+      {user.map((user) => (
+        <div className="travel-card travel-card__content">
+          <div className="travel-card__imagem-box">
+            <img
+              alt="Avatar Usuário"
+              src={user.avatar}
+              className="travel-card__imagem"
+            />
           </div>
-        </div>
-
-        {isLogado && isAdmin ? (
-          <>
-            <div className="btao-Container">
-              <a
-                href="/visualizarUsuario/<%= user[i].id %>"
-                className="btao-form"
-              >
-                <button className="btao-Container__btao-visualizar listarUsuarios">
-                  <i className="bi bi-eye-fill"></i>
-                </button>
-              </a>
-
-              <a
-                href="/listarUsuarios/<%= user[i].id %>/editar"
-                className="btao-form"
-              >
-                <button className="btao-Container__btao-editar listarUsuarios">
-                  <i className="bi bi-pencil-fill"></i>
-                </button>
-              </a>
-
-              <form
-                action="/listarUsuarios/<%= user[i].id %>/delete"
-                className="btao-form"
-                method="POST"
-              >
-                <button className="btao-Container__btao-deletar listarUsuarios">
-                  <i className="bi bi-trash-fill"></i>
-                </button>
-              </form>
+          <div className="travel-card__info-box">
+            <div className="travel-card__infoDesc">
+              <ul className="Propriedades">
+                <li className="usuario-card__propriedade">Nome</li>
+                <li className="usuario-card__dado">{user.nome}</li>
+                <li className="usuario-card__propriedade">Email </li>
+                <li className="usuario-card__dado">{user.email}</li>
+              </ul>
             </div>
-          </>
-        ) : null}
-      </div>
+          </div>
+
+          {isLogado && isAdmin ? (
+            <>
+              <div className="btao-Container">
+                <a
+                  href="/visualizarUsuario/<%= user[i].id %>"
+                  className="btao-form"
+                >
+                  <button className="btao-Container__btao-visualizar listarUsuarios">
+                    <i className="bi bi-eye-fill"></i>
+                  </button>
+                </a>
+
+                <a
+                  href="/listarUsuarios/<%= user[i].id %>/editar"
+                  className="btao-form"
+                >
+                  <button className="btao-Container__btao-editar listarUsuarios">
+                    <i className="bi bi-pencil-fill"></i>
+                  </button>
+                </a>
+
+                <form
+                  action="/listarUsuarios/<%= user[i].id %>/delete"
+                  className="btao-form"
+                  method="POST"
+                >
+                  <button className="btao-Container__btao-deletar listarUsuarios">
+                    <i className="bi bi-trash-fill"></i>
+                  </button>
+                </form>
+              </div>
+            </>
+          ) : null}
+        </div>
+      ))}
     </>
   );
 };
