2bdc24a0ee05487fe7f0190957b0924264381977
Anna Raphaela Berto <raphaelaberto@gmail.com>
2022-01-29 10:08:19 -0300

    feature: alterando formulario do perfil edit

--- client/src/pages/PerfilEdit/index.js
+++ client/src/pages/PerfilEdit/index.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import auth from "../../storage/auth";
 
 const PerfilEdit = ({ tokenUser }) => {
   const [user, setUser] = useState([]);
+  const [email, setEmail] = useState("");
+  const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [cpf, setCpf] = useState("");
+  const [cep, setCep] = useState("");
+  const [logradouro, setLogradouro] = useState("");
+  const [nascimento, setNascimento] = useState("");
+  const [complemento, setComplemento] = useState("");
+  const [localidade, setLocalidade] = useState("");
+  const [uf, setUf] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setCofirmarSenha] = useState("");
 
   const id = tokenUser.id;
-  console.log(id);
-  //   const { id } = useParams();
   const apiURL = "http://localhost:3333/visualizarUsuario/" + id;
   useEffect(() => {
     fetch(apiURL)
@@ -19,18 +29,48 @@ const PerfilEdit = ({ tokenUser }) => {
         })
       );
   }, []);
-  console.log(user);
+
+  //   Form
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("http://localhost:3333/visualizarUsuario/" + id, {
+        method: "post",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          senha,
+          confirmarSenha,
+          uf,
+          localidade,
+          complemento,
+          nascimento,
+          logradouro,
+          cep,
+          cpf,
+          telefone,
+          nome,
+        }),
+      });
+      res = await res.json();
+      //   auth.setToken(res.token);
+      window.location.href = "/";
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <main className="perfil">
         <div className="perfil__container-top">
           <h1 className="perfil__title">Meu Perfil</h1>
         </div>
-        <form
-          className="perfil__dados-do-usuario"
-          action="/Perfil/edit"
-          method="POST"
-        >
+        <form onSubmit={handleSubmit} className="perfil__dados-do-usuario">
           <div className="dados-do-usuario__img-container">
             <img className="img-container__avatar" src={user.avatar} />
             <button className="img-container__btn-enviar-img">
@@ -42,14 +82,28 @@ const PerfilEdit = ({ tokenUser }) => {
               <h2 className="info__titulo">Nome Completo</h2>
             </label>
             <div className="info__usuario">
-              <input id="nome" name="nome" type="text" value={user.nome} />
+              <input
+                id="nome"
+                name="nome"
+                type="text"
+                placeholder={user.nome}
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
+              />
             </div>
           </div>
           <div className="dados-do-usuario__info email">
             <label htmlFor="email">
               <h2 className="info__titulo">E-mail</h2>
             </label>
-            <input id="email" name="email" type="text" value={user.email} />
+            <input
+              id="email"
+              name="email"
+              type="text"
+              placeholder={user.email}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="dados-do-usuario__info nascimento">
             <label htmlFor="nascimento">
@@ -59,7 +113,9 @@ const PerfilEdit = ({ tokenUser }) => {
               id="nascimento"
               name="nascimento"
               type="date"
-              value={user.nascimento}
+              placeholder={user.nascimento}
+              value={nascimento}
+              onChange={(e) => setNascimento(e.target.value)}
             />
           </div>
           <div className="dados-do-usuario__info telefone">
@@ -70,20 +126,36 @@ const PerfilEdit = ({ tokenUser }) => {
               id="telefone"
               name="telefone"
               type="tel"
-              value={user.telefone}
+              placeholder={user.telefone}
+              value={telefone}
+              onChange={(e) => setTelefone(e.target.value)}
             />
           </div>
           <div className="dados-do-usuario__info cpf">
             <label htmlFor="cpf">
               <h2 className="info__titulo">CPF</h2>
             </label>
-            <input id="cpf" name="cpf" type="text" value={user.cpf} />
+            <input
+              id="cpf"
+              name="cpf"
+              type="text"
+              placeholder={user.cpf}
+              value={cpf}
+              onChange={(e) => setCpf(e.target.value)}
+            />
           </div>
           <div className="dados-do-usuario__info cep">
             <label htmlFor="cep">
               <h2 className="info__titulo">CEP</h2>
             </label>
-            <input id="cep" name="cep" type="text" value={user.cep} />
+            <input
+              id="cep"
+              name="cep"
+              type="text"
+              placeholder={user.cep}
+              value={cep}
+              onChange={(e) => setCep(e.target.value)}
+            />
           </div>
           <div className="dados-do-usuario__info endereco">
             <label htmlFor="endereco">
@@ -93,7 +165,9 @@ const PerfilEdit = ({ tokenUser }) => {
               id="endereco"
               name="endereco"
               type="text"
-              value={user.logradouro}
+              placeholder={user.logradouro}
+              value={logradouro}
+              onChange={(e) => setLogradouro(e.target.value)}
             />
           </div>
           <div className="dados-do-usuario__info complemento">
@@ -104,7 +178,9 @@ const PerfilEdit = ({ tokenUser }) => {
               id="complemento"
               name="complemento"
               type="text"
-              value={user.complemento}
+              placeholder={user.complemento}
+              value={complemento}
+              onChange={(e) => setComplemento(e.target.value)}
             />
           </div>
           <div className="dados-do-usuario__info cidade">
@@ -115,14 +191,23 @@ const PerfilEdit = ({ tokenUser }) => {
               id="cidade"
               name="cidade"
               type="text"
-              value={user.localidade}
+              placeholder={user.localidade}
+              value={localidade}
+              onChange={(e) => setLocalidade(e.target.value)}
             />
           </div>
           <div className="dados-do-usuario__info estado">
             <label htmlFor="estado">
               <h2 className="info__titulo">Estado</h2>
             </label>
-            <input id="estado" name="estado" type="text" value={user.uf} />
+            <input
+              id="estado"
+              name="estado"
+              type="text"
+              placeholder={user.uf}
+              value={uf}
+              onChange={(e) => setUf(e.target.value)}
+            />
           </div>
           <div className="dados-do-usuario__info senha">
             <label htmlFor="senha">
@@ -132,8 +217,9 @@ const PerfilEdit = ({ tokenUser }) => {
               id="senha"
               name="senha"
               type="password"
-              value=""
+              value={senha}
               placeholder="Insira somente para troca"
+              onChange={(e) => setSenha(e.target.value)}
             />
           </div>
           <div className="dados-do-usuario__info confirmar">
@@ -144,8 +230,9 @@ const PerfilEdit = ({ tokenUser }) => {
               id="confirmar"
               name="confirmar"
               type="password"
-              value=""
+              value={confirmarSenha}
               placeholder="Insira somente para troca"
+              onChange={(e) => setCofirmarSenha(e.target.value)}
             />
           </div>
           <input
